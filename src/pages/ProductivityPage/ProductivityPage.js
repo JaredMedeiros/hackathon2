f68a67a873bab22818fd9143f8796e4b0af4f601
Jared Medeiros <jared14jared@gmail.com>
2022-03-11 13:26:20 -0500

    added an analog clock and a weather widget

--- src/pages/ProductivityPage/ProductivityPage.js
+++ src/pages/ProductivityPage/ProductivityPage.js
@@ -1,8 +1,15 @@
 import './ProductivityPage.scss'
 import Pommodoro from '../../components/Pommodoro/Pommodoro.js'
+import Weather from '../../components/Weather/Weather.js'
+import CurrentTime from '../../components/CurrentTime/CurrentTime.js'
+
 
 export default function ProductivityPage() {
     return(
+        <>
+        <Weather/>
         <Pommodoro/>
+        <CurrentTime/>
+        </>
     )
 }
