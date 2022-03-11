0191a75fe76b56cc7e76ed7da36d5f3996d54ea5
Jared Medeiros <jared14jared@gmail.com>
2022-03-11 10:17:26 -0500

    initial styling for landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import LandingPage from './pages/LandingPage/LandingPage'
 
 function App() {
   return (
-    <div></div>
+    <LandingPage/>
   );
 }
 
---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,20 @@
+
+import './LandingPage.scss'
+
+
+export default function LandingPage() {
+    return (
+        <body class="landing">
+            <div class="landing-left">
+              <h1 class="landing-left__line-1">WELCOME. TODAY IS</h1>
+              <h1 class="landing-left__line-2">FRIDAY APRIL 11TH 2022</h1>
+              <h1 class="landing-left__line-3">LET'S GET STARTED</h1>
+              <h2>Jared Medeiros and Siar Wahidi</h2>
+            </div>
+            <div class="landing-right">
+              <h1 class="landing-right__1">WRKBLOCK</h1>
+              <h1 class="landing-right__2">ENTER</h1>
+            </div>
+        </body>
+    )
+}
